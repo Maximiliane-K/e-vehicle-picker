efaff33fabc496ddc054248c1911755aa2fd6a66
Maximiliane-K <>
2023-06-20 18:52:06 +0000

    Add validation to check if user input is valid

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ e-vehicle-survey-data spreadsheet
 import gspread
 from google.oauth2.service_account import Credentials
 
-# The following code was taken from the love_sandwiches walkthrough project by CodeInstitute
+# The following code was taken from the love_sandwiches 
+# walkthrough project by CodeInstitute
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -24,9 +25,9 @@ def intro():
     """
     print("Welcome to Electric Vehicle Picker!\n")
     print(("This app is meant to support you and your customer on the journey"
-        " of finding the right match when it comes to a new e-vehicle.\n"))
+           " of finding the right match when it comes to a new e-vehicle.\n"))
     print(("Follow the instructions given to get a recommendation and save"
-        " the customer data for further analysis.\n"))
+           " the customer data for further analysis.\n"))
 
 
 def get_customer_details():
@@ -34,10 +35,11 @@ def get_customer_details():
     Get the customers' full name, age, gender
     and the info if they already drive electric.
     """
-    print("Please enter the 4 customer details as discribed below sepperated by a comma without spaces.")
-    details = ["1. Full Name: enter customers first name and surname(,)", 
-               "2. Age: enter customers' age(,)",
-               "3. Gender: enter m,f or d(,)",
+    print(("Please enter the 4 customer details as discribed below"
+           "sepperated by a comma without spaces."))
+    details = ["1. Full Name: enter customers first name and surname", 
+               "2. Age: enter customers' age",
+               "3. Gender: enter m,f or d",
                "4. Already drive electric? : enter yes or no\n"]
     print(*details, sep = "\n\n")
     print("Example: June Austin,35,f,no\n")
@@ -56,7 +58,7 @@ def update_customer_details_worksheet(details):
     print("Updating customers worksheet...\n")
     customers_worksheet = SHEET.worksheet("customers")
     customers_worksheet.append_row(customer_details)
-    print("Customer details successfully added to customers worksheet\n")
+    print("Customer details successfully added to customers worksheet.\n")
 
 def select_car_type():
     """
@@ -66,9 +68,23 @@ def select_car_type():
     styles = ["a) Microcar", "b) Compact Car", "c) Hatchback", "d) SUV", "e) Sedan", "f) Cabrio\n"]
     print(*styles, sep = "\n")
 
-    style_choice = input("Please only select one letter [a, b, c, d, e, f]: ")
+    type_choices = input("Only select one letter [a, b, c, d, e, f]: \n")
 
-    customer_style = style_choice.lower().strip()
+    customer_choice = type_choices.lower().strip()
+    validate_style_choice(customer_choice)
+
+
+def validate_style_choice(customer_choice):
+    """
+    Function to validate if the user input given is valid.
+    """
+    try:
+        if customer_choice not in ["a", "b", "c", "d", "e", "f"]:
+            raise ValueError(
+                f"You entered '{customer_choice}'. \nOnly letters from a-f are valid"
+            )
+    except ValueError as e:
+        print(f"Entry not valid:\n{e}, please try again.")
     
 
 intro()
@@ -76,3 +92,4 @@ details = get_customer_details()
 customer_details = [detail for detail in details]
 update_customer_details_worksheet(customer_details)
 select_car_type()
+
